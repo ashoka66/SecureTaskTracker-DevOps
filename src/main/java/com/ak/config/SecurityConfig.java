f678be4b5162277d018cf56d65f7aafd5a997c6a
Gogulapati Ashok <124648330+ashoka66@users.noreply.github.com>
2025-11-16 21:52:11 +0530

    TL : Initial update in the company work flow in the servic.config

--- src/main/java/com/ak/config/SecurityConfig.java
+++ src/main/java/com/ak/config/SecurityConfig.java
@@ -18,8 +18,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	
 	//Define Inmemory users for simplicity
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
+	@Bean //TL updated method access member public to private 
+	private InMemoryUserDetailsManager userDetailsService() {
 		
 		UserDetails user = User.withUsername("admin").password("password").roles("USER").build();
 		return new InMemoryUserDetailsManager(user);
@@ -29,8 +29,8 @@ public class SecurityConfig {
 	
 	
 	//Define Security filterChain (authorization rules)
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+	@Bean //TL updated method access member public to private 
+	private SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
 		
 		http.authorizeHttpRequests(auth -> auth.requestMatchers("/login")
 				                                .permitAll()
@@ -48,7 +48,7 @@ public class SecurityConfig {
 	
 	//No password encoding for demo (never use in production)
 	@Bean
-	public static NoOpPasswordEncoder passwordEncoder() {
+	private static NoOpPasswordEncoder passwordEncoder() {
 		return(NoOpPasswordEncoder)  NoOpPasswordEncoder.getInstance();
 	}
 	
